37336307f9d5d78650a4f39772948bc73974491f
Norman Kerr 柯念北 <kenianbei@gmail.com>
2014-08-18 12:40:33 -0700

    Complete overhaul of js and file handling.  Adds ability to stream file chunks while recording. See notes.
    Adds video/audio stream recording for firefox (video only works in firefox nightly). This will cut down considerable on upload times.
    Media recorder only asks for mic permission on record (https://drupal.org/node/2232601).
    Better UI with help messages.
    Better error handling on both server and client sides.
    Added theme template for more customizability.
    Added better audio visualization and mic meter.

--- js/media-recorder.js
+++ js/media-recorder.js
@@ -1,21 +1,72 @@
 /**
  * @file
- * Adds an interface between the media recorder jQuery plugin and the drupal media module.
+ * Loads correct javascript files based on browser features. We do this because of namespace conflicts with external
+ * libraries.
  */
 
 (function($) {
-  Drupal.behaviors.mediaRecorder = {
-    attach: function(context, settings) {
-      $('.media-recorder-wrapper').mediaRecorder({
-        'recordingPath': Drupal.settings.mediaRecorder.recordingPath,
-        'filePath': Drupal.settings.mediaRecorder.filePath,
-        'fileName': Drupal.settings.mediaRecorder.fileName,
-        'timeLimit': Drupal.settings.mediaRecorder.timeLimit,
-        'width': Drupal.settings.mediaRecorder.width,
-        'height': Drupal.settings.mediaRecorder.height,
-        'swfurl': Drupal.settings.mediaRecorder.swfurl,
-        'html5url': Drupal.settings.mediaRecorder.html5url,
+  'use strict';
+
+  // Add mediaRecorder object to Drupal.
+  Drupal.mediaRecorder = Drupal.mediaRecorder || {};
+
+  // Normalize features.
+  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+  window.AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext;
+  window.URL = window.URL || window.webkitURL;
+  Drupal.mediaRecorder.origin = window.location.origin || window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port: '');
+
+  // Feature detection.
+  var getUserMediaCheck = typeof(navigator.getUserMedia) === 'function';
+  var mediaRecorderCheck = typeof(window.MediaRecorder) === 'function';
+  var webAudioCheck = typeof(window.AudioContext) === 'function';
+  var swfobjectCheck = typeof(window.swfobject) === 'object';
+  var flashVersionCheck = swfobjectCheck ? (swfobject.getFlashPlayerVersion().major >= 10) : false;
+
+  // Check to see that browser can use the recorder.
+  if ((getUserMediaCheck && webAudioCheck) || (flashVersionCheck && swfobjectCheck)) {
+
+    // Use the MediaRecorder API. Currently only works in firefox.
+    if (getUserMediaCheck && webAudioCheck && mediaRecorderCheck) {
+      $.ajax({
+        url: Drupal.settings.basePath + Drupal.settings.mediaRecorder.modulePath + '/media-recorder-api.js',
+        async: false,
+        dataType: 'script'
       });
     }
-  };
+
+    // Use HTML5 features (Web Audio API).
+    else if (getUserMediaCheck && webAudioCheck && !mediaRecorderCheck) {
+      $.ajax({
+        url: Drupal.settings.basePath + Drupal.settings.mediaRecorder.html5url + '/recorder.js',
+        async: false,
+        dataType: 'script'
+      });
+      $.ajax({
+        url: Drupal.settings.basePath + Drupal.settings.mediaRecorder.modulePath + '/media-recorder-html5.js',
+        async: false,
+        dataType: 'script'
+      });
+    }
+
+    // Use Flash.
+    else if (flashVersionCheck) {
+      $.ajax({
+        url: Drupal.settings.basePath + Drupal.settings.mediaRecorder.swfurl + '/recorder.js',
+        async: false,
+        dataType: 'script'
+      });
+      $.ajax({
+        url: Drupal.settings.basePath + Drupal.settings.mediaRecorder.modulePath + '/media-recorder-flash.js',
+        async: false,
+        dataType: 'script'
+      });
+    }
+  }
+
+  // Otherwise just use the basic file field.
+  else {
+    $('.field-widget-media-recorder').find('.media-recorder-wrapper').hide();
+  }
+
 })(jQuery);
