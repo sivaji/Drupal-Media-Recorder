568e24e1dc59765346788528c3ef33b2547b4ae8
Sivaji <sivaji2009@gmail.com>
2020-06-30 21:38:58 +0530

    Removed unwanted code & method rename.

--- src/Element/MediaRecorder.php
+++ src/Element/MediaRecorder.php
@@ -14,8 +14,6 @@ use Drupal\file\Element\ManagedFile;
 /**
  * Provides a media recorder render element.
  *
- * @see render_example_theme()
- *
  * @RenderElement("media_recorder")
  */
 class MediaRecorder extends RenderElement {
@@ -29,14 +27,14 @@ class MediaRecorder extends RenderElement {
     return [
       '#input' => TRUE,
       '#process' => [
-        [$class, 'media_recorder_element_process'],
+        [$class, 'elementProcess'],
       ],
       '#pre_render' => [
         [$class, 'preRender'],
       ],
-      '#value_callback' => [$class, 'media_recorder_element_value'],
+      '#value_callback' => [$class, 'elementValue'],
       '#element_validate' => [
-        [$class, 'media_recorder_element_validate'],
+        [$class, 'elementValidate'],
       ],
       '#default_value' => NULL,
       '#extended' => TRUE,
@@ -51,10 +49,9 @@ class MediaRecorder extends RenderElement {
   }
 
   /**
-   * Add comment
+   * Render API callback: Attaches the requires JS Libraries.
    */
   public static function preRender($element) {
-    // dpm(__METHOD__);
     $element['#attached']['library'][] = 'media_recorder/swfobject';
     $element['#attached']['library'][] = 'media_recorder/FlashWavRecorder';
     $element['#attached']['library'][] = 'media_recorder/Recorderjs';
@@ -66,23 +63,18 @@ class MediaRecorder extends RenderElement {
   }
 
   /**
-   * The #value_callback for a media_recorder type element.
-   *
-   * @see media_recorder_element_info()
+   * {@inheritdoc}
    */
-  public static function media_recorder_element_value(&$element, $input = FALSE, $form_state = NULL) {
-    // dpm(__METHOD__);
+  public static function elementValue(&$element, $input = FALSE, $form_state = NULL) {
+
     $fid = 0;
 
     // Find the current value of this field from the form state.
     $form_state_fid = $form_state->getValues();
-    // dpm($form_state_fid);
-    // dpm($element);
-    // print_r($form_state_fid);exit;
+
     foreach ($element['#parents'] as $parent) {
       $form_state_fid = isset($form_state_fid[$parent]) ? $form_state_fid[$parent] : 0;
     }
-
 
     if (is_numeric($form_state_fid)) {
       $fid = $form_state_fid;
@@ -91,11 +83,8 @@ class MediaRecorder extends RenderElement {
     // Process any input and attach files.
     if ($input !== FALSE) {
       $return = $input;
-      // return $return;
-      // print_r($return);exit;
-      // Uploads take priority over all other values.
-      if ($files = file_managed_file_save_upload($element, $form_state)) {
 
+      if ($files = file_managed_file_save_upload($element, $form_state)) {
         if ($element['#multiple']) {
           $fids = array_merge($fids, array_keys($files));
         }
@@ -128,11 +117,11 @@ class MediaRecorder extends RenderElement {
     else {
       if ($element['#extended']) {
         $default_fid = isset($element['#default_value']['fid']) ? $element['#default_value']['fid'] : 0;
-        $return = isset($element['#default_value']) ? $element['#default_value'] : array('fid' => 0);
+        $return = isset($element['#default_value']) ? $element['#default_value'] : ['fid' => 0];
       }
       else {
         $default_fid = isset($element['#default_value']) ? $element['#default_value'] : 0;
-        $return = array('fid' => 0);
+        $return = ['fid' => 0];
       }
 
       // Confirm that the file exists when used as a default value.
@@ -140,24 +129,16 @@ class MediaRecorder extends RenderElement {
         $fid = $file->id();
       }
     }
-
-
     $return['fids'] = $fid;
-    // dpm($return);
-    // $return['fids'] = 18;
     return $return;
   }
 
-
-
-
   /**
-   * Process callback for the media_recorder form element.
+   * Render API callback: Expands the media_recorder element type.
    *
-   * @see media_recorder_element_info()
+   * Provides player for recording & playback.
    */
-  function media_recorder_element_process(&$element, FormStateInterface $form_state, &$complete_form) {
-    // dpm(__METHOD__);
+  function elementProcess(&$element, FormStateInterface $form_state, &$complete_form) {
     $settings = media_recorder_get_settings();
     $fid = isset($element['#value']['fids']) ? $element['#value']['fids'] : 0;
 
@@ -166,12 +147,12 @@ class MediaRecorder extends RenderElement {
 
     // If the element as a default file, add the absolute url to the file.
     if (!empty($fid) && !is_object($fid) && $file = file_load($fid)) {
-      // print_r($file);exit;
-      // $file->url = file_create_url($file->uri);
+
       $file->url = file_create_url($file->get('uri')->value);
 
       $file_obj = new \stdClass();
       $file_obj->fid = $file->id();
+      // FIXME
       $file_obj->type = 'audio'; //$file->get('bundle')->value;
       $file_obj->filemime = $file->getMimeType();
       $file_obj->url = $file->url;
@@ -185,100 +166,60 @@ class MediaRecorder extends RenderElement {
 
     // Add the javascript based recorder, which will be shown only if the browser
     // supports the required features.
-    $element['recorder'] = array(
+    $element['recorder'] = [
       '#prefix' => '<div id="' . $id . '" class="media-recorder-wrapper" style="display: none;">',
       '#suffix' => '</div>',
       '#theme' => 'media_recorder',
-    );
+    ];
 
     // Add a fallback upload element for devices unable to use the recorder.
-    $element['fallback'] = array(
+    $element['fallback'] = [
       '#type' => 'managed_file',
       '#id' => $id . '-fallback',
-      '#description' => t('Allowed extensions: @extensions', array('@extensions' => $element['#upload_validators']['file_validate_extensions'][0])),
+      '#description' => t('Allowed extensions: @extensions', ['@extensions' => $element['#upload_validators']['file_validate_extensions'][0]]),
       '#default_value' => $fid,
       '#upload_location' => $element['#upload_location'],
       '#upload_validators' => $element['#upload_validators'],
-    );
+    ];
 
     $element['upload_button'] = [
-    //   // '#name' => $parents_prefix . '_upload_button',
        '#type' => 'submit',
        '#value' => t('Upload'),
        '#attributes' => ['class' => ['js-hide']],
        '#validate' => [],
        '#submit' => ['file_managed_file_submit'],
-    //   // '#limit_validation_errors' => [$element['#parents']],
-    //   // '#ajax' => $ajax_settings,
-    //   // '#weight' => -5,
+    // '#limit_validation_errors' => [$element['#parents']],
+    // '#ajax' => $ajax_settings,
+    // '#weight' => -5,
     ];
 
     // Add a hidden fid field for storing the returned recorded file using ajax.
-    $element['fids'] = array(
+    $element['fids'] = [
       '#type' => 'hidden',
       '#value' => $fid,
-      '#attributes' => array('id' => array($id . '-fids')),
-    );
-    $element['fid'] = array(
-      '#type' => 'hidden',
-      '#value' => $fid,
-      '#attributes' => array('id' => array($id . '-fid')),
-    );
-
-    // '#submit' => ['file_managed_file_submit'],
+      '#attributes' => ['id' => [$id . '-fids']],
+    ];
 
     // Add javascript libraries.
-    $element['#attached'] = array(
-      // 'libraries_load' => array(
-      //   array('swfobject'),
-      //   array('FlashWavRecorder'),
-      //   array('Recorderjs'),
-      // ),
-      // 'js' => array(
-      'drupalSettings' => array(
-        // 'media-recorder-api' => array(
-        //   'type' => 'file',
-        //   'data' => drupal_get_path('module', 'media_recorder') . '/js/media-recorder-api.js',
-        //   'scope' => 'footer',
-        // ),
-        // 'media-recorder-html5' => array(
-        //   'type' => 'file',
-        //   'data' => drupal_get_path('module', 'media_recorder') . '/js/media-recorder-html5.js',
-        //   'scope' => 'footer',
-        // ),
-        // 'media-recorder-flash' => array(
-        //   'type' => 'file',
-        //   'data' => drupal_get_path('module', 'media_recorder') . '/js/media-recorder-flash.js',
-        //   'scope' => 'footer',
-        // ),
-        // 'media-recorder' => array(
-        //   'type' => 'file',
-        //   'data' => drupal_get_path('module', 'media_recorder') . '/js/media-recorder.js',
-        //   'scope' => 'footer',
-        // ),
-        // array(
-        //   'type' => 'setting',
-        //   'data' => array(
-            'mediaRecorder' => array(
-              'swfPath' => libraries_get_path('FlashWavRecorder'),
-              'workerPath' => libraries_get_path('Recorderjs'),
-              'elements' => array(
-                array(
-                  'id' => $id,
-                  'conf' => array(
-                    'time_limit' => $element['#time_limit'],
-                    'constraints' => $element['#constraints'],
-                    'file' => $file_obj,
-                    'upload_location' => $element['#upload_location'],
-                  ),
-                ),
-              ),
-            ),
-          // ),
-          // 'scope' => 'header',
-        // ),
-      ),
-    );
+    $element['#attached'] = [
+      'drupalSettings' => [
+        'mediaRecorder' => [
+          'swfPath' => libraries_get_path('FlashWavRecorder'),
+          'workerPath' => libraries_get_path('Recorderjs'),
+          'elements' => [
+            [
+              'id' => $id,
+              'conf' => [
+                'time_limit' => $element['#time_limit'],
+                'constraints' => $element['#constraints'],
+                'file' => $file_obj,
+                'upload_location' => $element['#upload_location'],
+              ],
+            ],
+          ],
+        ],
+      ],
+    ];
 
     // Add custom css if enabled.
     if ($settings['css']) {
@@ -358,7 +299,7 @@ class MediaRecorder extends RenderElement {
         // );
       }
       catch (Exception $e) {
-        \Drupal::logger('media_kaltura')->error('There was a problem connecting to the kaltura server: @error', array('@error' => $e->getMessage()));
+        \Drupal::logger('media_kaltura')->error('There was a problem connecting to the kaltura server: @error', ['@error' => $e->getMessage()]);
       }
     }
     return $element;
@@ -366,37 +307,27 @@ class MediaRecorder extends RenderElement {
 
 
   /**
-   * Validate media_recorder form elements.
-   *
-   * @see media_recorder_element_info()
+   * Render API callback: Validates the media_recorder element.
    */
-  function media_recorder_element_validate($element, FormStateInterface $form_state) {
-    // dpm(__METHOD__);
+  function elementValidate($element, FormStateInterface $form_state) {
 
     $fid = isset($element['#value']['fid']) ? $element['#value']['fid'] : $element['#value']['fallback']['fid'];
-    // dpm($fid);
+
     // Check required property based on the FID.
     if ($element['#required'] && empty($fid)) {
-      // form_error($element, t('%name is a required field.', array('%name' => $element['#title'])));
       $form_state->setError($element, t('%name is a required field.', ['%name' => $element['#title']]));
     }
 
     if (!empty($fid) && !is_object($fid) && $file = file_load($fid)) {
-      // $scheme = \Drupal::service("file_system")->uriScheme($file->uri);
       $scheme = \Drupal::service("file_system")->uriScheme($file->get('uri')->value);
-
 
       // Validate local files.
       if ($scheme === 'public' || $scheme === 'private') {
-        // $element['#upload_validators']['file_validate_extensions'][] = 'ogg';
         $file_validate_errors = file_validate($file, $element['#upload_validators']);
         if ($file_validate_errors) {
-          // form_error($element, implode('<br />', $file_validate_errors));
           $form_state->setError($element, implode('<br />', $file_validate_errors));
         }
       }
     }
   }
-
-
 }
